6e8958659657f3a202bc336a493ab8101b164a36
FeuerSturm <94806347+FeuerSturm@users.noreply.github.com>
2022-02-06 19:25:12 +0100

    fixed folder, updated plugin
    - renamed "Reddragon" to "Redragon"
    - removed redundant function from ORIOS Mousepad

--- plugins/Speedlink/Speedlink ORIOS RGB Gaming Mousepad.js
+++ plugins/Speedlink/Speedlink ORIOS RGB Gaming Mousepad.js
@@ -24,10 +24,6 @@ var vLedPositions = [
 	[1,0], [0,0], [0,1], [0,2], [1,2], [2,2], [3,2], [3,1], [3,0], [2,0]
 ];
 
-var vLedMapping = [
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-];
-
 export function LedNames()
 {
   return vLedNames;
