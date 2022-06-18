9e13698fe74109c1e39870205a32e766868cc90c
Naitoshedo <sjakubiec@gmail.com>
2022-06-18 14:10:07 -0500

    Example Music App Batch Files
    The following batch files will allow you to set an effect + layout and then launch your favorite music playing application.

---
+++ automation/Shortcut-Launching-Examples/Music-Apps/Plexamp.bat
@@ -0,0 +1,22 @@
+@echo off
+REM
+REM You will need to set the layout you have created for Audio Visualizers
+REM
+explorer "signalrgb://layout/apply/Pump%%20Up%%20Beats?-silentlaunch-"
+
+REM We need to wait at least 2 seconds before firing the next SignalRGB command
+timeout 2
+
+REM
+REM You will need to make sure an Audio Visual Effect is installed
+REM
+REM "Ripple%%20Visualizer", "Fire%%20Visualizer", "Rave%%20Visualizer%, "Particle%%20Visualizer",
+REM "Tesla%%20Coil", "Sunset%%20Visualizer", "Sunrise%%20Visualizer", "Sonic%%20Bubbles", "RGBarz",
+REM "Bars%%20Visualizer", or "Pump%%20Up%%20Beats"
+REM
+REM Are the current values (available effects right now
+REM
+explorer "signalrgb://effect/apply/Pump%%20Up%%20Beats?-silentlaunch-"
+
+REM Start the program
+start "Plexamp" /B %localappdata%\Programs\plexamp\Plexamp.exe
