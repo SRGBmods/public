3d240c5a5814f35d0311e568e6022bc1a52636a7
Naitoshedo <sjakubiec@gmail.com>
2022-06-18 14:26:43 -0500

    Cleanup
    Update description for referencing current game in scripts

--- automation/Shortcut-Launching-Examples/Games/PUBG.bat
+++ automation/Shortcut-Launching-Examples/Games/PUBG.bat
@@ -1,6 +1,6 @@
 @echo off
 REM
-REM You will need to set the layout you have created for Destiny 2 (Or a generic Game layout)
+REM You will need to set the layout you have created for PUBG (Or a generic Game layout)
 REM
 explorer "signalrgb://layout/apply/PUBG?-silentlaunch-"
 
@@ -8,7 +8,7 @@ REM We need to wait at least 2 seconds before firing the next SignalRGB command
 timeout 2
 
 REM
-REM You must have the Destiny 2 Effect installed for this to work
+REM You must have the PUBG: Battlegrounds Effect installed for this to work
 REM SignalRGB Pro is of course required for this.
 REM
 explorer "signalrgb://effect/apply/PUBG:%%20Battlegrounds?-silentlaunch-"
--- automation/Shortcut-Launching-Examples/Games/Rainbow Six Siege.bat
+++ automation/Shortcut-Launching-Examples/Games/Rainbow Six Siege.bat
@@ -1,6 +1,6 @@
 @echo off
 REM
-REM You will need to set the layout you have created for Destiny 2 (Or a generic Game layout)
+REM You will need to set the layout you have created for Rainbow Six: Siege (Or a generic Game layout)
 REM
 explorer "signalrgb://layout/apply/Rainbow%%20Six%%20Siege?-silentlaunch-"
 
@@ -8,7 +8,7 @@ REM We need to wait at least 2 seconds before firing the next SignalRGB command
 timeout 2
 
 REM
-REM You must have the Destiny 2 Effect installed for this to work
+REM You must have the Rainbow Six: Siege Effect installed for this to work
 REM SignalRGB Pro is of course required for this.
 REM
 explorer "signalrgb://effect/apply/Rainbow%%20Six:%%20Siege?-silentlaunch-"
