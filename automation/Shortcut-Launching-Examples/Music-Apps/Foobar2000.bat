643ee60a4c6b19ce5798578c2bc07457fac93899
Naitoshedo <sjakubiec@gmail.com>
2022-09-24 14:17:12 -0500

    Exit detection added

--- automation/Shortcut-Launching-Examples/Music-Apps/Foobar2000.bat
+++ automation/Shortcut-Launching-Examples/Music-Apps/Foobar2000.bat
@@ -1,4 +1,7 @@
 @echo off
+REM Do not change the following line (this launches the batch script minimized):
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+
 REM -------------------------------------------------------------------------------------
 REM If you are using the "while media is playing, show" rule, please change RULE=media
 REM otherwise place keep it at always
@@ -12,7 +15,7 @@ REM ----------------------------------------------------------------------------
 REM
 REM Remember %%20 = a space
 REM Edit the next line for your layout:
-explorer "signalrgb://layout/apply/Pump%%20Up%%20Beats?-silentlaunch-"
+set LAYOUT=Pump%%20Up%%20Beats
 
 REM -------------------------------------------------------------------------------------
 REM You will need to make sure an Audio Visual Effect is installed
@@ -20,8 +23,8 @@ REM "Ripple%%20Visualizer", "Fire%%20Visualizer", "Rave%%20Visualizer%, "Particl
 REM "Tesla%%20Coil", "Sunset%%20Visualizer", "Sunrise%%20Visualizer", "Sonic%%20Bubbles", "RGBarz",
 REM "Bars%%20Visualizer", or "Pump%%20Up%%20Beats"
 REM -------------------------------------------------------------------------------------
-timeout 2 > nul 2> nul
-explorer "signalrgb://effect/apply/Pump%%20Up%%20Beats?-silentlaunch-"
+set VEFFECT=Pump%%20Up%%20Beats
+
 
 REM -------------------------------------------------------------------------------------
 REM -------------------------------------------------------------------------------------
@@ -29,7 +32,6 @@ REM ----------------------------------------------------------------------------
 REM --------------------------------Do not edit anything below!--------------------------
 REM -------------------------------------------------------------------------------------
 REM -------------------------------------------------------------------------------------
-goto launchapp
 
 IF %RULE% == always (goto saveall)
 IF %RULE% == media (goto savestatic)
@@ -43,14 +45,25 @@ FOR /F "skip=2 tokens=2,*" %%A IN ('reg query "HKEY_CURRENT_USER\SOFTWARE\Whirlw
 goto layoutsave
 
 :LAYOUTSAVE
-FOR /F "skip=2 tokens=2,*" %%A IN ('reg query "HKEY_CURRENT_USER\SOFTWARE\WhirlwindFX\SignalRgb\layouts" /v "always"') DO set "CurrentLayout=%%B" > nul 2> nul
-goto applylayout
+FOR /F "skip=2 tokens=2,*" %%A IN ('reg query "HKEY_CURRENT_USER\SOFTWARE\WhirlwindFX\SignalRgb\layouts" /v "currentLayout"') DO set "CurrentLayout=%%B" > nul 2> nul
+goto gamelaunch
 
-:LAUNCHAPP
+:gamelaunch
 REM Start the program
-start "Foobar 2000" /B "C:\Program Files (x86)\foobar2000\foobar2000.exe"
-echo Press any key to return to previous effects
-pause > nul 2> nul
+explorer "signalrgb://layout/apply/%Layout%?-silentlaunch-"
+timeout 2 > nul 2> nul
+explorer "signalrgb://effect/apply/%VEFFECT%?-silentlaunch-"
+start "Foobar 2000" /B "C:\Program Files\foobar2000\foobar2000.exe"
+timeout 2 > nul 2> nul
+goto exitcheck
+
+:exitcheck
+tasklist /fi "imagename eq foobar2000.exe"|find /i "=========================" >nul 2>nul &&(
+w32tm /stripchart /computer:localhost /period:10 /dataonly /samples:2  1>nul
+goto exitcheck
+)
+
 explorer "signalrgb://layout/apply/%CurrentLayout%?-silentlaunch-"
 timeout 2 > nul 2> nul
 explorer "signalrgb://effect/apply/%CurrentEffect%?-silentlaunch-" 
+exit
